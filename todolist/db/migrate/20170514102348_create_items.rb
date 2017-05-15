fc9a399d5ae0eb5923010ce6abdbee8737bcb70d
Taha Saleh <taha.a.saleh@gmail.com>
2017-05-15 20:49:50 +0200

    Add t.references :user in item migration

--- todolist/db/migrate/20170514102348_create_items.rb
+++ todolist/db/migrate/20170514102348_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.0]
     create_table :items do |t|
       t.text :description
       t.boolean :done, default: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- todolist/db/schema.rb
+++ todolist/db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 20170515162435) do
   create_table "items", force: :cascade do |t|
     t.text     "description"
     t.boolean  "done",        default: false
+    t.integer  "user_id"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
