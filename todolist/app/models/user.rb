12376ee80ec3a876eed38fdf5eaa2e9114a2217a
Taha Saleh <taha.a.saleh@gmail.com>
2017-05-15 20:20:39 +0200

    Delete devise migration and used tokenauth migration

--- todolist/app/models/user.rb
+++ todolist/app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :items
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 end
