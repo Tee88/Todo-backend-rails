5c4b34192567eb8918a3dfe856fc54512a54ebca
Taha Saleh <taha.a.saleh@gmail.com>
2017-05-21 15:22:04 +0200

    Work on devise token auth

--- todolist/app/controllers/application_controller.rb
+++ todolist/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::MimeResponds
+  # beacuse the user model has no controler in which 
+  #we would permit it's params, we permit them here in the application controller.
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    end
+  # the function above can be named anything else.
+  # this will basically run before going to the devis_controller which controles the
+  # user model in our case. so we pass the any other custom attributes which were not
+  # included in the devise user migration file.
 end
--- todolist/db/schema.rb
+++ todolist/db/schema.rb
@@ -33,19 +33,15 @@ ActiveRecord::Schema.define(version: 20170515162435) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.string   "user_name",              default: "",      null: false
     t.string   "email",                  default: "",      null: false
     t.text     "tokens"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+    t.index [nil], name: "index_users_on_confirmation_token", unique: true
   end
 
 end
